10b71c1ea54333ed4bfeb94b856439cc11fa89f1
Graham Cox <graham@grahamcox.co.uk>
2015-10-21 17:05:03 +0100

    Initial support for converting between units.

---
+++ food-service/src/main/units/convert.js
@@ -0,0 +1,18 @@
+/**
+ * Calculate a conversion from one unit to another unit
+ * @param {Unit} from The unit to convert from
+ * @param {Unit} to The unit to convert to
+ * @param {Number} qty The amount to convert. Defaults to 1 if not set.
+ * @return {Number} The converted amount
+ */
+function convert(from, to, qty) {
+    const realqty = qty || 1;
+    
+    const fromScalingFactor = from.scalingFactor;
+    const toScalingFactor = to.scalingFactor;
+    const scalingFactor = fromScalingFactor / toScalingFactor;
+    const scaled = realqty * scalingFactor;
+    return scaled;
+}
+
+module.exports = convert;
---
+++ food-service/src/test/units/convert-spec.js
@@ -0,0 +1,36 @@
+const expect = require('chai').expect;
+const Unit = require('../../main/units/unit');
+const convert = require('../../main/units/convert');
+
+describe('units', function() {
+    describe('#convert()', function () {
+        const kgs = new Unit({
+            id: 'kg',
+            type: 'weight',
+            system: 'metric',
+            scalingFactor: 1000
+        });
+        const lbs = new Unit({
+            id: 'lb',
+            type: 'weight',
+            system: 'imperial',
+            scalingFactor: 453.592
+        });
+        
+        it('should produce the correct scaling factor from Kg to Lb', () => {
+            expect(convert(kgs, lbs)).to.be.within(2.2046, 2.2047);
+        });
+        
+        it('should produce the correct scaling factor from Lb to Kg', () => {
+            expect(convert(lbs, kgs)).to.be.within(0.4535, 0.4536);
+        });
+        
+        it('should correctly convert 2Kg to Lb', () => {
+            expect(convert(kgs, lbs, 2)).to.be.within(4.4092, 4.4093);
+        });
+        
+        it('should correctly convert 2Lb to Kg', () => {
+            expect(convert(lbs, kgs, 2)).to.be.within(0.9071, 0.9072);
+        });
+    });
+});
